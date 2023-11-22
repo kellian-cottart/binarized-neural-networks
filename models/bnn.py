50dadd3ebeb33d1a4e608c10d504364986bf0348
kellianoy <kellian.cottart@edu.ece.fr>
2023-11-22 12:43:40 +0100

    refactor: bnn

--- models/bnn.py
+++ models/bnn.py
@@ -1,9 +1,10 @@
 
 import torch
 from .layers import *
+from .dnn import *
 
 
-class BNN(torch.nn.Module):
+class BNN(DNN):
     """ Binarized Neural Network (BNN)
 
     Neural Network with binary weights and activations, using hidden weights called "degrees of certainty" (DOCs) to approximate real-valued weights.
@@ -12,43 +13,19 @@ class BNN(torch.nn.Module):
 networks
     """
 
-    def __init__(self, layers=[512], init='gauss', std=0.01, device='cuda', latent_weights=True, dropout=False):
-        """ Initialize BNN
+    def __init__(self, *args, **kwargs):
+        self.latent_weights = kwargs['latent_weights'] if 'latent_weights' in kwargs else True
+        super().__init__(*args, **kwargs)
 
-        Args:
-            layers (list): List of layer sizes (including input and output layers)
-            init (str): Initialization method for weights
-            std (float): Standard deviation for initialization
-            device (str): Device to use for computation (e.g. 'cuda' or 'cpu')
-            latent_weights (bool): Whether to use latent weights or not
-            dropout (bool): Whether to use dropouts layers before each layer
-
-        """
-        super(BNN, self).__init__()
-        self.n_layers = len(layers)-2
-        self.layers = torch.nn.ModuleList()
-        self.device = device
-
-        ### LAYER INITIALIZATION ###
+    def _layer_init(self, layers, dropout=False, bias=False):
         for i in range(self.n_layers+1):
+            # Linear layers with BatchNorm
             if dropout:
-                self.layers.append(torch.nn.Dropout(p=0.2))
+                layers.append(torch.nn.Dropout(p=0.2))
             self.layers.append(BinarizedLinear(
-                layers[i], layers[i+1], bias=False, device=device, latent_weights=latent_weights))
+                layers[i], layers[i+1], bias=bias, device=self.device, latent_weights=self.latent_weights))
             self.layers.append(torch.nn.BatchNorm1d(
-                layers[i+1], affine=True, track_running_stats=True, device=device))
-
-        ### WEIGHT INITIALIZATION ###
-        for layer in self.layers:
-            if hasattr(layer, 'weight'):
-                if init == 'gauss':
-                    torch.nn.init.normal_(
-                        layer.weight, mean=0.0, std=std)
-                elif init == 'uniform':
-                    torch.nn.init.uniform_(
-                        layer.weight, a=-std/2, b=std/2)
-                elif init == 'xavier':
-                    torch.nn.init.xavier_normal_(layer.weight)
+                layers[i+1], affine=not bias, track_running_stats=True, device=self.device))
 
     def forward(self, x):
         """Forward propagation of the binarized neural network
--- models/dnn.py
+++ models/dnn.py
@@ -3,44 +3,61 @@ from tqdm import trange
 
 
 class DNN(torch.nn.Module):
-    """ Neural Network (DNN) 
-
-    Neural Network with real-valued weights and activations.
+    """ Neural Network Base Class
     """
 
-    def __init__(self, layers=[512], init='normal', std=0.01, device='cuda'):
-        """ Initialize DNN
+    def __init__(self, layers=[512], init='normal', std=0.01, device='cuda', dropout=False, bias=False, *args, **kwargs):
+        """ NN initialization
 
         Args: 
             layers (list): List of layer sizes (including input and output layers)
             init (str): Initialization method for weights
             std (float): Standard deviation for initialization
             device (str): Device to use for computation (e.g. 'cuda' or 'cpu')
-
+            dropout (bool): Whether to use dropout
         """
         super(DNN, self).__init__()
         self.n_layers = len(layers)-2
-        self.layers = torch.nn.ModuleList()
         self.device = device
-
+        self.layers = torch.nn.ModuleList()
         ### LAYER INITIALIZATION ###
+        self._layer_init(layers, dropout, bias)
+        ### WEIGHT INITIALIZATION ###
+        self._weight_init(init, std)
+
+    def _layer_init(self, layers, dropout=False, bias=False):
+        """ Initialize layers of NN
+
+        Args:
+            dropout (bool): Whether to use dropout
+            bias (bool): Whether to use bias
+        """
         for i in range(self.n_layers+1):
             # Linear layers with BatchNorm
+            if dropout:
+                layers.append(torch.nn.Dropout(p=0.2))
             self.layers.append(torch.nn.Linear(
-                layers[i], layers[i+1], bias=False, device=device))
+                layers[i], layers[i+1], bias=bias, device=self.device))
             self.layers.append(torch.nn.BatchNorm1d(
-                layers[i+1], affine=True, track_running_stats=True, device=device))
+                layers[i+1], affine=not bias, track_running_stats=True, device=self.device))
 
-        ### WEIGHT INITIALIZATION ###
-        for layer in self.layers[::2]:
-            if init == 'gauss':
-                torch.nn.init.normal_(
-                    layer.weight, mean=0.0, std=std)
-            elif init == 'uniform':
-                torch.nn.init.uniform_(
-                    layer.weight, a=-std/2, b=std/2)
-            elif init == 'xavier':
-                torch.nn.init.xavier_normal_(layer.weight)
+    def _weight_init(self, init='normal', std=0.01):
+        """ Initialize weights of each layer
+
+        Args:
+            init (str): Initialization method for weights
+            std (float): Standard deviation for initialization
+        """
+        for layer in self.layers:
+            if hasattr(layer, 'weight'):
+                if init == 'gauss':
+                    torch.nn.init.normal_(
+                        layer.weight, mean=0.0, std=std)
+                elif init == 'uniform':
+                    torch.nn.init.uniform_(
+                        layer.weight, a=-std/2, b=std/2)
+                elif init == 'xavier':
+                    torch.nn.init.xavier_normal_(layer.weight)
 
     def forward(self, x):
         """ Forward pass of DNN
@@ -53,10 +70,8 @@ class DNN(torch.nn.Module):
 
         """
         ### FORWARD PASS ###
-        # For each pair of layers (linear + batchnorm)
-        for linear, batchnorm in zip(self.layers[::2], self.layers[1::2]):
-            x = linear(x)
-            x = batchnorm(x)
-            if batchnorm != self.layers[-1]:
+        for layer in self.layers:
+            x = layer(x)
+            if layer is not self.layers[-1] and isinstance(layer, torch.nn.BatchNorm1d):
                 x = torch.nn.functional.relu(x)
         return x
