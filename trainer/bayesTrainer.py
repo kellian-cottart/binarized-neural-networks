61a0971fff3a806b7f1d9254b47ca4d4852672bb
kellianoy <kellian.cottart@edu.ece.fr>
2023-11-30 08:46:24 +0100

    feat: updated bayesTrainer for mcmc sampling

--- trainer/bayesTrainer.py
+++ trainer/bayesTrainer.py
@@ -1,4 +1,5 @@
 from .closureTrainer import ClosureTrainer
+import torch
 
 
 class BayesTrainer(ClosureTrainer):
@@ -12,6 +13,61 @@ class BayesTrainer(ClosureTrainer):
         **kwargs: Arbitrary keyword arguments (most likely optimizer or scheduler parameters)
     """
 
+    def __init__(self, *args, **kwargs):
+        if "test_mcmc_samples" in kwargs["training_parameters"]:
+            self.test_mcmc_samples = kwargs["training_parameters"]["test_mcmc_samples"]
+        else:
+            raise ValueError(
+                "BayesTrainer needs test_mcmc_samples to be defined in training_parameters (number of samples to use for the monte carlo prediction)")
+
+        super().__init__(*args, **kwargs)
+
     def fit(self, *args, **kwargs):
         super().fit(*args, **kwargs)
         self.optimizer.update_prior_lambda()
+
+    def test(self, inputs, labels):
+        """Test the model on the given inputs and labels
+
+        Args:
+            inputs (torch.Tensor): Input data
+            labels (torch.Tensor): Labels
+
+        Returns:
+            torch.Tensor: Predictions
+        """
+        with torch.no_grad():
+            noise = []
+            for _ in range(self.test_mcmc_samples):
+                noise.append(torch.bernoulli(
+                    torch.sigmoid(2*self.optimizer.state["lambda"])))
+            predictions = self.monte_carlo_prediction(inputs, noise)
+            idx_pred = torch.argmax(predictions, dim=1)
+        return len(torch.where(idx_pred == labels)[0]) / len(labels)
+
+    def monte_carlo_prediction(self, inputs, noise):
+        """Perform a monte carlo prediction on the given inputs
+
+        Args:
+            inputs (torch.Tensor): Input data
+            noise (torch.Tensor, optional): Noise to use for the prediction. Defaults to None.
+
+        Returns:
+            torch.Tensor: Predictions
+        """
+        # Retrieve the parameters
+        parameters = self.model.parameters()
+        predictions = []
+
+        # If no noise is given, we generate a new one
+        if len(noise) == 0:
+            noise.append(torch.where(self.optimizer.state['mu'] <= 0, torch.zeros_like(
+                self.optimizer.state['mu']), torch.ones_like(self.optimizer.state['mu'])))
+
+        # We iterate over the parameters
+        for n in noise:
+            # 2p - 1
+            torch.nn.utils.vector_to_parameters(2*n-1, parameters)
+            predictions.append(self.model.forward(inputs).to(self.device))
+
+        return torch.stack(predictions, dim=2).mean(dim=2)
